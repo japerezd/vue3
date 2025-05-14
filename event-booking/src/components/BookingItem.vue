7383e729fa9bce0cfe93abce5084dd63ed0cbe2a
Jorge Perez <jorge_perez@homedepot.com>
2025-05-13 21:50:03 -0600

    Add variant to RoundButton

--- event-booking/src/components/BookingItem.vue
+++ event-booking/src/components/BookingItem.vue
@@ -1,11 +1,17 @@
 <template>
   <SectionCard>
-    {{ description }}
+    <div class="flex justify-between">
+      <div>{{ description }}</div>
+      <RoundButton variant="danger">
+        Cancel
+      </RoundButton>
+    </div>
   </SectionCard>
 </template>
 
 <script setup>
 import SectionCard from './SectionCard.vue';
+import RoundButton from './RoundButton.vue';
 
 defineProps({
   description: String
