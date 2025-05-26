d60b093f39f8a15e1dfdc159c2be321bbc3cec74
Jorge Perez <jorge_perez@homedepot.com>
2025-05-26 13:15:54 -0600

    Add mock server

--- event-booking/src/App.vue
+++ event-booking/src/App.vue
@@ -5,7 +5,13 @@
     <h1 class="text-4xl font-medium">Event Booking App</h1>
     <h2 class="text-2xl font-medium">All Events</h2>
     <section class="grid grid-cols-2 gap-8">
-      <EventCard v-for="i in 8" :key="i" title="Vue Conference 2025" when="2025-05-01" description="Conference about Vue and JavaScript" @register="console.log('Registered!')" />
+      <template v-if="!eventsAreLoading">
+        <EventCard v-for="event in events" :key="event.id" :title="event.title" :when="event.date"
+        :description="event.description" @register="console.log('Registered!')" />
+      </template>
+      <template v-else>
+        <LoadingEventCard v-for="i in 4" :key="i" />
+      </template>
     </section>
     <h2 class="text-2xl font-medium">Your Bookings</h2>
 
@@ -19,7 +25,23 @@
 
 
 <script setup>
+import { onMounted, ref } from 'vue';
 import EventCard from '@/components/EventCard.vue';
 import BookingItem from '@/components/BookingItem.vue';
+import LoadingEventCard from './components/LoadingEventCard.vue';
 
+const events = ref([]);
+const eventsAreLoading = ref(false);
+
+const fetchEvents = async () => {
+  eventsAreLoading.value = true;
+  try {
+    const response = await fetch('http://localhost:3001/events');
+    events.value = await response.json();
+  } finally {
+    eventsAreLoading.value = false;
+  }
+}
+
+onMounted(() => fetchEvents());
 </script>
