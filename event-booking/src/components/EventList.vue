50509dabb4ff1c76fe5991914a3edce3a9198ce4
Jorge Perez <jorge_perez@homedepot.com>
2025-05-31 01:01:19 -0600

    Handle error

--- event-booking/src/components/EventList.vue
+++ event-booking/src/components/EventList.vue
@@ -1,5 +1,16 @@
 <template>
-<section class="grid grid-cols-2 gap-8">
+  <template v-if="error">
+    <SectionCard>
+      <div class="space-y-4 items-center flex flex-col">
+        <div class="text-red-500">
+          Could not load events at the moment. Please try again later.
+        </div>
+        <RoundButton @click="fetchEvents">Retry Now</RoundButton>
+      </div>
+    </SectionCard>
+  </template>
+<template v-else>
+  <section class="grid grid-cols-2 gap-8">
       <template v-if="!loading">
         <template v-if="events.length">
           <EventCard v-for="event in events" :key="event.id" :title="event.title" :when="event.date"
@@ -17,22 +28,29 @@
       </template>
     </section>
 </template>
+</template>
 
 <script setup>
 import { onMounted, ref } from 'vue';
 import LoadingEventCard from '@/components/LoadingEventCard.vue';
 import EventCard from '@/components/EventCard.vue';
+import SectionCard from './SectionCard.vue';
+import RoundButton from './RoundButton.vue';
 
 defineEmits(['register']);
 
 const events = ref([]);
 const loading = ref(false);
+const error = ref(null);
 
 const fetchEvents = async () => {
   loading.value = true;
+  error.value = null;
   try {
     const response = await fetch('http://localhost:3001/events');
     events.value = await response.json();
+  } catch (err) {
+    error.value = err;
   } finally {
     loading.value = false;
   }
