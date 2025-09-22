e5ec133169b834d3df5dcf997c593d2e5a615b94
Jorge Perez <jorgeperez@MacBook-Air-de-Jorge.local>
2025-09-22 00:10:09 -0600

    Create FadeTransition component

---
+++ vue-settings/src/components/FadeTransition.vue
@@ -0,0 +1,18 @@
+<template>
+  <Transition mode="out-in" name="fade">
+    <slot></slot>
+  </Transition>
+</template>
+
+<style>
+/* we will explain what these classes do next! */
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.2s ease;
+}
+/* fade- comes from given name to Transition. If not given, then use v- */
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
