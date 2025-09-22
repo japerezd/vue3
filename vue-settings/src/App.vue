23a9eaaca349709851d8dd4c3a7caab55ea7c9b8
Jorge Perez <jorgeperez@MacBook-Air-de-Jorge.local>
2025-09-22 00:05:00 -0600

    Add transition

--- vue-settings/src/App.vue
+++ vue-settings/src/App.vue
@@ -1,8 +1,57 @@
-<script setup lang="ts"></script>
-
 <template>
-  <main>
-    Hello
+  <main class="max-w-2xl mx-auto px-4">
+    <nav class="font-medium text-center text-gray-500 border-b border-gray-200 mb-4">
+      <ul class="flex flex-wrap -mb-px">
+        <li v-for="tab in tabs" :key="tab.key">
+          <TabLink :current-tab="currentTab" :tab="tab" @click="currentTab = tab.key" />
+        </li>
+      </ul>
+    </nav>
+    <Transition mode="out-in" name="fade">
+      <component :is="currentTabComponent" />
+    </Transition>
   </main>
 </template>
 
+<script setup lang="ts">
+import { computed, ref } from 'vue';
+import type { Tab, TabKey } from './types';
+import TabLink from './components/TabLink.vue';
+import GeneralSettings from './components/GeneralSettings.vue';
+import NotificationsSettings from './components/NotificationsSettings.vue';
+import PrivacySettings from './components/PrivacySettings.vue';
+
+const tabs: Tab[] = [
+  {
+    key: 'General',
+    label: 'General',
+    component: GeneralSettings
+  },
+  {
+    key: 'Notifications',
+    label: 'Notifications',
+    component: NotificationsSettings
+  },
+  {
+    key: 'Privacy',
+    label: 'Privacy',
+    component: PrivacySettings
+  }
+]
+
+const currentTab = ref<TabKey>('General')
+const currentTabComponent = computed(() => tabs.find(tab => tab.key === currentTab.value)?.component)
+</script>
+
+<style scoped>
+/* we will explain what these classes do next! */
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.2s ease;
+}
+/* fade- comes from given name to Transition. If not given, then use v- */
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+</style>
---
+++ vue-settings/src/components/TabLink.vue
@@ -0,0 +1,30 @@
+<template>
+  <button
+    @click="$emit('click')"
+    :class="{
+      'p-4 border-b-2': true,
+      'border-transparent': !isActive,
+      'border-b-blue-600 text-blue-600': isActive,
+      
+      'cursor-pointer': true
+    }"
+  >
+    {{ tab.label }}
+  </button>
+
+</template>
+
+<script setup lang="ts">
+import type { Tab } from '@/types';
+import { computed } from 'vue';
+
+const isActive = computed(() => props.currentTab === props.tab.key);
+const props = defineProps<{
+  currentTab: string;
+  tab: Tab;
+}>();
+
+defineEmits<{
+  click: []; // No parameters
+}>();
+</script>
