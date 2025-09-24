58b1144df189758bdbee42141a14bbf7e62192b7
Jorge Perez - M4 <jorge_alberto1997@hotmail.com>
2025-09-23 21:54:57 -0600

    Add types for settings

--- vue-settings/src/composables/useSettings.ts
+++ vue-settings/src/composables/useSettings.ts
@@ -1,46 +1,59 @@
-import { ref, watch } from "vue";
+import { ref, watch } from 'vue';
 
 interface GeneralSettings {
   username: string;
   email: string;
   about: string;
   gender: string;
-  country: string
+  country: string;
 }
+
+interface SettingsMap {
+  general: GeneralSettings;
+  privacy: PrivacySettings;
+  notifications: NotificationsSettings;
+}
+
+type SettingsKey = keyof SettingsMap
+
+const init = <T extends SettingsKey>(key: T, defaults: SettingsMap[T]) => {
+  const stored = localStorage.getItem(key);
+
+  return stored !== null ? JSON.parse(stored) : defaults;
+};
+
+const watcher =
+  <T extends SettingsKey>(key: T) =>
+  (value: SettingsMap[T]) => {
+    localStorage.setItem(key, JSON.stringify(value));
+  };
 
 const general = ref<GeneralSettings>(
   // IIFE to initialize the data
-  (() => {
-  const stored = localStorage.getItem('general');
-
-  return stored !== null
-    ? JSON.parse(stored)
-    : {
-        about: '',
-        country: 'Mexico',
-        gender: 'male',
-        email: '',
-        username: '',
-      };
-})());
-
-watch(
-  general,
-  (value) => {
-    localStorage.setItem('general', JSON.stringify(value));
-  },
-  { deep: true },
+  init('general', {
+    about: '',
+    country: 'Mexico',
+    gender: 'male',
+    email: '',
+    username: '',
+  }),
 );
+
+watch(general, watcher('general'), { deep: true });
 
 interface NotificationsSettings {
   email: boolean;
   sms: boolean;
 }
 
-const notifications = ref<NotificationsSettings>({
-  email: false,
-  sms: false
-});
+const notifications = ref<NotificationsSettings>(
+  init('notifications', {
+    email: false,
+    sms: false,
+  }),
+);
+
+watch(notifications, watcher('notifications'), { deep: true })
 
 interface PrivacySettings {
   visibility: Visibility;
@@ -49,15 +62,19 @@ interface PrivacySettings {
 
 type Visibility = 'public' | 'private';
 
-const privacy = ref<PrivacySettings>({
-  searchEngineIndexing: false,
-  visibility: 'public'
-});
+const privacy = ref<PrivacySettings>(
+  init('privacy', {
+    searchEngineIndexing: false,
+    visibility: 'public',
+  }),
+);
+
+watch(privacy, watcher('privacy'), { deep: true })
 
 export function useSettings() {
   return {
     general,
     notifications,
-    privacy
-  }
+    privacy,
+  };
 }
