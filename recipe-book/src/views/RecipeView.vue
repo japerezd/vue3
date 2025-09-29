f3b0f07bde8bcfa718bad59f7a3d7cd4396e93c1
Jorge <jorge_alberto1997@hotmail.com>
2025-09-28 20:01:54 -0600

    Move button inside div

--- recipe-book/src/views/RecipeView.vue
+++ recipe-book/src/views/RecipeView.vue
@@ -4,10 +4,10 @@
     <p class="mb-4">{{ recipe?.description }}</p>
     <div class="flex items-center gap-4">
       <RouterLink :to="{ name: 'edit-recipe', params: { id: recipe?.id }}" class="hover:underline">Edit</RouterLink>
+      <button v-if="recipe" @click="recipeStore.toggleFavorite(recipe.id)" class="px-4 py-2 bg-orange-600 text-white rounded hover:bg-orange-700">
+        {{ isFavorite ? 'Remove from favorites' : 'Add to favorites' }}
+      </button>
     </div>
-    <button v-if="recipe" @click="recipeStore.toggleFavorite(recipe.id)" class="px-4 py-2 bg-orange-600 text-white rounded hover:bg-orange-700">
-      {{ isFavorite ? 'Remove from favorites' : 'Add to favorites' }}
-    </button>
   </div>
   
 </template>
